6f004ef43534524bc00f08dfa2c250e763947e2a
thesaddest <youngscarlxrd@yandex.by>
2022-08-04 21:39:52 +0200

    add: auth flow

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException,
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserEntity } from '../user/user.entity'
+import { Repository } from 'typeorm'
+import { JwtService } from '@nestjs/jwt'
+import { AuthDto } from './auth.dto'
+import { compare, hash, genSalt } from 'bcryptjs'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		@InjectRepository(UserEntity)
+		private readonly userRepository: Repository<UserEntity>,
+		private readonly jwtService: JwtService,
+	) {}
+
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto)
+
+		return {
+			user: this.returnUserFields(user),
+			accessToken: await this.issueAccessToken(user.id),
+		}
+	}
+
+	async register(dto: AuthDto) {
+		const localUser = await this.userRepository.findOneBy({ email: dto.email })
+		if (localUser) throw new BadRequestException('Email is already registered')
+
+		const salt = await genSalt(10)
+		const newUser = await this.userRepository.create({
+			email: dto.email,
+			password: await hash(dto.password, salt),
+		})
+
+		const user = await this.userRepository.save(newUser)
+		return {
+			user: this.returnUserFields(user),
+			accessToken: await this.issueAccessToken(user.id),
+		}
+	}
+
+	async validateUser(dto: AuthDto) {
+		const user = await this.userRepository.findOne({
+			where: { email: dto.email },
+			select: ['id', 'email', 'password'],
+		})
+
+		if (!user) throw new NotFoundException('User was not found.')
+
+		const isValidPassword = await compare(dto.password, user.password)
+		if (!isValidPassword) throw new UnauthorizedException('Wrong password!')
+
+		return user
+	}
+
+	async issueAccessToken(userId: number) {
+		const data = {
+			id: userId,
+		}
+		return await this.jwtService.signAsync(data, {
+			expiresIn: '31d',
+		})
+	}
+
+	returnUserFields(user: UserEntity) {
+		return {
+			id: user.id,
+			email: user.email,
+		}
+	}
+}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { Module } from '@nestjs/common'
+import { UserService } from './user.service'
+import { UserController } from './user.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserEntity } from './user.entity'
+import { SubscriptionsEntity } from './subscriptions.entity'
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService]
+	controllers: [UserController],
+	providers: [UserService],
+	imports: [TypeOrmModule.forFeature([UserEntity, SubscriptionsEntity])],
 })
 export class UserModule {}
