4decb5f4856373db16d163221ea2e552bda0a0f2
thesaddest <youngscarlxrd@yandex.by>
2022-09-04 09:32:51 +0200

    structure and .env

--- frontend/app/components/pages/home/Home.tsx
+++ frontend/app/components/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import Layout from "@/components/layout/Layout";
-import Discover from "@/components/pages/discover/Discover";
-import Catalog from "@/components/pages/catalog/Catalog";
+import Discover from "@/components/pages/home/discover/Discover";
+import Catalog from "@/components/pages/home/catalog/Catalog";
 
 const Home: FC = () => {
 	return (
