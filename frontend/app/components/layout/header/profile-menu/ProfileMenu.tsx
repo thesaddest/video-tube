1f9234a9ae54054deff259b211a79bf85006eca3
thesaddest <youngscarlxrd@yandex.by>
2022-09-11 16:13:20 +0200

    add: header/profile-menu

--- frontend/app/components/layout/header/profile-menu/ProfileMenu.tsx
+++ frontend/app/components/layout/header/profile-menu/ProfileMenu.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
 import { useAuth } from "@/hooks/useAuth";
 import { api } from "@/store/api/api";
+import { useOutside } from "@/hooks/useOutside";
+import { useActions } from "@/hooks/useActions";
+import styles from "./ProfileMenu.module.scss";
+import Image from "next/image";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
+import Link from "next/link";
 
 const ProfileMenu: FC = () => {
 	const { user } = useAuth();
@@ -9,9 +15,46 @@ const ProfileMenu: FC = () => {
 		skip: !user
 	});
 
+	const { isShown, setIsShown, ref } = useOutside(false);
+	const { logout } = useActions();
+
 	if (isLoading) return null;
 
-	return <div>{data?.name}</div>;
+	return (
+		<div ref={ref} className={styles.wrapper}>
+			<button onClick={() => setIsShown(!isShown)}>
+				<Image
+					src={data?.avatarPath || ""}
+					alt={data?.name}
+					width={40}
+					height={40}
+					priority
+				/>
+				<span className={styles.name}>{data?.name}</span>
+				{isShown ? <GoChevronUp /> : <GoChevronDown />}
+			</button>
+
+			{isShown && (
+				<div className={styles["profile-menu"]}>
+					<ul>
+						<li>
+							<Link href={`/c/${user?.id}`}>
+								<a>My Channel</a>
+							</Link>
+						</li>
+						<li>
+							<Link href={"/studio"}>
+								<a>Studio</a>
+							</Link>
+						</li>
+						<li>
+							<button onClick={logout}>Logout</button>
+						</li>
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default ProfileMenu;
--- frontend/app/components/layout/header/upload-video/UploadVideo.tsx
+++ frontend/app/components/layout/header/upload-video/UploadVideo.tsx
@@ -1,7 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { videoApi } from "@/store/api/video.api";
+import stylesIcon from "../icons-right/IconsRight.module.scss";
+import { HiUpload } from "react-icons/hi";
 
 const UploadVideo: FC = () => {
-	return <div>UploadVideo</div>;
+	const [isOpen, setIsOpen] = useState(false);
+	const [videoId, setVideoId] = useState<number>(0);
+
+	const [createVideo, { isLoading }] = videoApi.useCreateVideoMutation();
+
+	return (
+		<>
+			<button
+				className={stylesIcon.button}
+				disabled={isLoading}
+				onClick={() => {
+					createVideo()
+						.unwrap()
+						.then(id => {
+							setVideoId(+id);
+							setIsOpen(true);
+						});
+				}}
+			>
+				<HiUpload />
+			</button>
+		</>
+	);
 };
 
 export default UploadVideo;
