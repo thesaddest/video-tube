815dd0c484a5ec6e7883a155dc91f324402bc0a4
thesaddest <youngscarlxrd@yandex.by>
2022-09-19 19:18:56 +0200

    add: player functionality

---
+++ frontend/app/components/pages/video/Video.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import Layout from "@/components/layout/Layout";
+import styles from "./Video.module.scss";
+import VideoPlayer from "@/components/pages/video/video-player/VideoPlayer";
+import cn from "classnames";
+import { useRouter } from "next/router";
+import { IVideo } from "@/types/video.interface";
+import { videoApi } from "@/store/api/video.api";
+
+const Video: FC = () => {
+	const { query } = useRouter();
+
+	const { data: video = {} as IVideo } = videoApi.useGetVideoByIdQuery(
+		Number(query.id),
+		{
+			skip: !query?.id
+		}
+	);
+	return (
+		<Layout title={video.name}>
+			<div className={styles.layout}>
+				<VideoPlayer videoPath={video.videoPath} />
+				<div></div>
+			</div>
+			<div className={cn(styles.layout, "mt-7")}></div>
+		</Layout>
+	);
+};
+
+export default Video;
---
+++ frontend/pages/v/[id].tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import Video from "@/components/pages/video/Video";
+
+const VideoPage: NextPage = props => {
+	return <Video {...props} />;
+};
+
+export default VideoPage;
